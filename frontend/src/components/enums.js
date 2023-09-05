42538ac33b1f03630ac2ac0c9049db173ad3bde7
heroyooki <heroyooki@gmail.com>
2023-09-05 18:58:04 +0300

    Added frontend service

---
+++ frontend/src/components/enums.js
@@ -0,0 +1,21 @@
+export const DATETIME_FORMAT = "MMM DD, YYYY, hh:mm A";
+
+export const EVENT_NAMES = {
+  new_connection: "new_connection",
+  lost_connection: "lost_connection",
+  heartbeat: "Heartbeat",
+};
+
+export const STATION_STATUS = {
+  available: "available",
+  offline: "offline",
+  reserved: "reserved",
+  charging: "charging",
+};
+
+export const STATION_STATUS_COLOR = {
+  available: "orange",
+  offline: "#cfd2ce",
+  reserved: "#2edbfa",
+  charging: "#66DA26",
+};
