d1e4e4d916936f455a57f78385d9b12cbaf51f2c
heroyooki <heroyooki@gmail.com>
2023-09-06 09:14:49 +0300

    Fixed cors origin requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,38 +3,49 @@ import vue from "@vitejs/plugin-vue";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // Utilities
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  optimizeDeps: {
-    exclude: ["vuetify"],
-  },
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-      styles: {
-        configFile: "src/styles/settings.scss",
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    optimizeDeps: {
+      exclude: ["vuetify"],
+    },
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+      vuetify({
+        autoImport: true,
+        styles: {
+          configFile: "src/styles/settings.scss",
+        },
+      }),
+    ],
+    devServer: {
+      proxy: {
+        "": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
-    }),
-  ],
-  define: { "process.env": {} },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  },
-  server: {
-    hmr: {
-      host: "localhost",
-      port: 5000,
-      protocol: "ws",
+    define: { "process.env": {} },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
     },
-  },
-});
+    server: {
+      hmr: {
+        host: "localhost",
+        port: 5000,
+        protocol: "ws",
+      },
+    },
+  });
+};
