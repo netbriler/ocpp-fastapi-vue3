2358477e6a2dcb82c9736b4d9972e99bae7ad6f0
heroyooki <heroyooki@gmail.com>
2023-09-18 15:49:42 +0300

    Fixed empty idTagInfo during authorization

--- backend/manager/services/ocpp/authorize.py
+++ backend/manager/services/ocpp/authorize.py
@@ -1,3 +1,5 @@
+from ocpp.v16.enums import AuthorizationStatus
+
 from charge_point_node.models.authorize import AuthorizeEvent
 from manager.models.tasks.authorize import AuthorizeTask
 
@@ -6,5 +8,5 @@ async def process_authorize(session, event: AuthorizeEvent) -> AuthorizeTask:
     return AuthorizeTask(
         message_id=event.message_id,
         charge_point_id=event.charge_point_id,
-        id_tag_info={}
+        id_tag_info={"status": AuthorizationStatus.accepted.value}
     )
